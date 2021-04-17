4dc7b9a6308bb62322fdf1bfa547c69b86020e59
atom-box <atom_box@hotmail.com>
2021-04-17 10:31:48 -0500

    Randomize customer ID and # of items. Remove unused config.

--- ndungu/orders.php
+++ ndungu/orders.php
@@ -6,60 +6,55 @@ error_reporting(E_ALL);
 
 require("./DBTransaction.php");
 
-// config
-$db_host = "database_host"; // e.g. 127.0.0.1
-$db_name = "database_name"; // example_store
-$db_user = "database_user"; // misterromney
-$db_password = "PASSWORD";  // 50000000000soooooooooosecret
-
-$customer_id = 2;
+$customer_id = random_int(1000, 9999);
 
 // ordering three items
 $products[] = [
     'product_id' => 1,
     'price' => 25.50,
-    'quantity' => 1
+    'quantity' => random_int(1,4)
   ];
   $products[] = [
     'product_id' => 2,
     'price' => 13.90,
-    'quantity' => 3
+    'quantity' => random_int(1,4)
   ];
   $products[] = [
     'product_id' => 3,
     'price' => 45.30,
-    'quantity' => 2
+    'quantity' => random_int(1,4)
   ];
+  $dollarsTotal = $products[0]['quantity'] * $products[0]['price'] + $products[1]['quantity'] * $products[1]['price'] + $products[2]['quantity'] * $products[2]['price'];
   
   // ping the db: you there?  make me an object
-  $transaction = new DBTransaction($db_host, $db_user, $db_password, $db_name);
+  $transaction = new DBTransaction();
 
   // gonna need to touch two tables and do these insertions in them
-$order_query = "
-insert into orders 
-(
-    customer_id, 
-    order_date, 
-    order_total
-) values(
-    :customer_id, 
-    :order_date, 
-    :order_total)
-";
+  $order_query = "
+  insert into orders 
+  (
+      customer_id, 
+      order_date, 
+      order_total
+  ) values(
+      :customer_id, 
+      :order_date, 
+      :order_total)
+  ";
 
-  $product_query = "
-  insert into orders_products 
-    (
-      order_id, 
-      product_id, 
-      price, 
-      quantity
-    ) values(
-        :order_id, 
-        :product_id, 
-        :price, 
-        :quantity
-    )";
+    $product_query = "
+    insert into orders_products 
+      (
+        order_id, 
+        product_id, 
+        price, 
+        quantity
+      ) values(
+          :order_id, 
+          :product_id, 
+          :price, 
+          :quantity
+      )";
   
   // save the ORDER, THEN ASK THE SQL WHICH SERIAL ID WAS IT?
   $transaction->insertQuery(
@@ -67,7 +62,7 @@ insert into orders
     [
     'customer_id' => $customer_id,
     'order_date' => date("Y-m-d H:i:s", time()),
-    'order_total' => 157.8
+    'order_total' => $dollarsTotal
     ]);
   
   $order_id = $transaction->last_insert_id;
